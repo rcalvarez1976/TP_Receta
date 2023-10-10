1dd1c0cf44e36a2e5a1e86ebe0565d185b99c3b4
Roberto Alvarez <rcalvarez1976@gmail.com>
2023-10-10 17:57:50 -0300

    Trabajo practico de receta con HTML y CSS

---
+++ index.js
@@ -0,0 +1,5 @@
+let nombre = prompt("Por favor ingresa tu nombre");
+
+let bienvenida = "desde \"Recetas Fáciles\" queremos darte la BIENVENIDA!!! a nuestra página. No olvides SUSCRIBIRTE!!!";
+
+ alert("Hola" + " " + nombre + "," + " " + bienvenida);
